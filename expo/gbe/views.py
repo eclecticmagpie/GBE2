528a758a5f5bd339c544b28f6c7fd0f0ae38b7a2
bethlakshmi <bethlakshmi@gmail.com>
2014-08-21 12:51:02 -0400

    Fix conficts.
    These are the two with conflicts that needed manual resolve.

--- expo/gbe/views.py
+++ expo/gbe/views.py
@@ -21,7 +21,7 @@ def index(request):
         try:
             profile = request.user.profile
         except Profile.DoesNotExist:
-            context_dict['alerts']= "You seem to have screwed up the registration. Contact Scratch"
+            context_dict['alerts']= "You seem to have screwed up the registration. Contact registration@burlesque-expo.com"
             return render_to_response ('gbe/index_unregistered_user.tmpl', context_dict)
         template = loader.get_template('gbe/index_registered_user.tmpl')
         context_dict['profile'] = profile
@@ -51,6 +51,7 @@ def landing_page(request):
                                    'acts': viewer_profile.get_acts(),
                                    'shows': viewer_profile.get_shows(),
                                    'classes': viewer_profile.is_teaching(),
+                                   'vendors': Vendor.objects.filter(profile = viewer_profile),
                                    'review_items': viewer_profile.bids_to_review()
                                })
     else:
@@ -197,7 +198,7 @@ def edit_persona(request, persona_id):
     if persona.performer_profile != profile:
         return HttpResponseRedirect('/')  # just fail for now    
     if request.method == 'POST':
-        form = PersonaForm(request.POST, instance=persona)
+        form = PersonaForm(request.POST, request.FILES, instance=persona)
         if form.is_valid():
             performer = form.save(commit=True)
             return HttpResponseRedirect('/')  
@@ -239,11 +240,22 @@ def bid_act(request):
     except Profile.DoesNotExist:
         return HttpResponseRedirect('/accounts/profile/')
     personae = profile.personae.all()
+    draft_fields = Act().bid_draft_fields
+    
     if len(personae) == 0:
         return HttpResponseRedirect("/performer/create?next=/act/create")
     if request.method == 'POST':
-        form = ActEditForm(request.POST, 
+        '''
+        If this is a formal submit request, then do all the checking.
+        If this is a draft, only a few fields are needed, use a form with fewer
+        required fields (same model)
+        '''
+        if 'submit' in request.POST.keys():
+            form = ActEditForm(request.POST, 
                           prefix='theact')
+        else:
+            form = ActEditDraftForm(request.POST, 
+                          prefix='theact')	    
         if  form.is_valid():
             act = form.save(commit=False)
             techinfo = TechInfo()
@@ -264,11 +276,14 @@ def bid_act(request):
                 return HttpResponseRedirect('/performer/create?next=/act/edit/'+str(act.id))
 
         else:
+            fields, requiredsub = Act().bid_fields
             return render (request,
                            'gbe/bid.tmpl',
                            {'forms':[form ], 
                             'page_title': page_title,                            
-                            'view_title': view_title, 
+                            'view_title': view_title,
+                            'draft_fields': draft_fields,
+                            'submit_fields': requiredsub
                     })  
 
         if 'submit' in request.POST.keys():
@@ -278,7 +293,8 @@ def bid_act(request):
                                'gbe/bid.tmpl',
                                {'forms':[form], 
                                 'page_title': page_title,                            
-                                'view_title': view_title, 
+                                'view_title': view_title,
+                                'draft_fields': draft_fields,
                                 'errors':problems})
                 
             else:
@@ -290,13 +306,10 @@ def bid_act(request):
                            'bid':act}
                 return render(request, 
                               'gbe/submission.tmpl',
-                              {'user':request.user,
-                               'bid':act,
-                               'bid_type':'act',
-                               })
-    
+                              compute_submission(details))
         else:
-            return HttpResponseRedirect('/fail1')  
+            return HttpResponseRedirect('/')
+
     else:
         form = ActEditForm(initial = {'owner':profile,
                                      'performer': personae[0]}, 
@@ -308,7 +321,8 @@ def bid_act(request):
                        'gbe/bid.tmpl',
                        {'forms':[form], 
                         'page_title': page_title,                            
-                        'view_title': view_title, 
+                        'view_title': view_title,
+                        'draft_fields': draft_fields
                         })
 
 @login_required
@@ -332,8 +346,26 @@ def edit_act(request, act_id):
         return HttpResponseRedirect('/fail2')  # just fail for now
     audio_info = act.tech.audio
     stage_info = act.tech.stage
+    draft_fields = Act().bid_draft_fields
+
     if request.method == 'POST':
-        form = ActEditForm(request.POST,  
+        '''
+        If this is a formal submit request, then do all the checking.
+        If this is a draft, only a few fields are needed, use a form with fewer
+        required fields (same model)
+        '''
+        if 'submit' in request.POST.keys():
+            form = ActEditForm(request.POST,  
+                           instance=act, 
+                           prefix = 'theact', 
+                           initial = { 
+                               'track_title':audio_info.track_title,
+                               'track_artist':audio_info.track_artist,
+                               'track_duration':audio_info.track_duration,
+                               'act_duration':stage_info.act_duration
+                           })
+        else:
+            form = ActEditDraftForm(request.POST,  
                            instance=act, 
                            prefix = 'theact', 
                            initial = { 
@@ -359,12 +391,15 @@ def edit_act(request, act_id):
             form.save()
 #            return HttpResponseRedirect('/wtf')
         else:
+            fields, requiredsub = Act().bid_fields
             return render (request,
                            'gbe/bid.tmpl',
                            {'forms':[form],
                             'page_title': page_title,                            
                             'view_title': view_title, 
-                        })
+                            'draft_fields': draft_fields,
+                            'submit_fields': requiredsub
+                       })
 
 
 
@@ -374,10 +409,10 @@ def edit_act(request, act_id):
                 return render (request,
                                'gbe/bid.tmpl',
                                {'forms':[form], 
-                                'page_title': page_title,                            
-                                'view_title': view_title, 
-                                'errors':problems})
-                
+                               'page_title': page_title,                            
+                               'view_title': view_title,
+                               'draft_fields': draft_fields,
+                               'errors':problems})
             else:
                 act.submitted = True
                 act.save()
@@ -390,7 +425,7 @@ def edit_act(request, act_id):
                     
 
         else:
-            return HttpResponseRedirect('/notsubmit')  
+            return HttpResponseRedirect('/')
     else:
         audio_info = act.tech.audio
         stage_info = act.tech.stage
@@ -405,11 +440,13 @@ def edit_act(request, act_id):
                            })
         form.fields['performer']= forms.ModelChoiceField(queryset=Performer.
                                                          objects.filter(contact=profile))  
+ 
         return render (request, 
                        'gbe/bid.tmpl',
                        {'forms':[form],
                         'page_title': page_title,                            
-                        'view_title': view_title, 
+                        'view_title': view_title,
+                        'draft_fields': draft_fields
                         })
                     
 
@@ -564,11 +601,23 @@ def bid_class(request):
         owner = request.user.profile
     except Profile.DoesNotExist:
         return HttpResponseRedirect('/accounts/profile/')
+    
     teachers = owner.personae.all()
+    draft_fields = Class().get_draft_fields
+
     if len (teachers) == 0 :
         return HttpResponseRedirect('/performer/create?next=/class/create')
     if request.method == 'POST':
-        form = ClassBidForm(request.POST)
+        '''
+        If this is a formal submit request, then do all the checking.
+        If this is a draft, only a few fields are needed, use a form with fewer
+        required fields (same model)
+        '''
+        if 'submit' in request.POST.keys():
+            form = ClassBidForm(request.POST)
+        else:
+            form = ClassBidDraftForm(request.POST)
+
         if form.is_valid():
             new_class = form.save(commit=True)
             if 'submit' in request.POST.keys():
@@ -580,29 +629,37 @@ def bid_class(request):
                     return render (request, 
                                    'gbe/bid.tmpl', 
                                    {'forms':[form], 
+                                   ' page_title': page_title,                            
+                                    'view_title': view_title,
+                                    'draft_fields': draft_fields,
                                     'errors':['Cannot submit, class is not complete']})
             new_class.save()
             return HttpResponseRedirect('/profile')
         else:
+            fields, requiredsub = Class().get_bid_fields
             return render (request, 
                            'gbe/bid.tmpl', 
                            {'forms':[form],
                             'page_title': page_title,                            
-                            'view_title': view_title, 
-                        })
+                            'view_title': view_title,
+                            'draft_fields': draft_fields,
+                            'submit_fields': requiredsub
+                            })
+
     else:
-        form = ClassBidForm (initial = {'owner':owner, })
+        form = ClassBidForm (initial = {'owner':owner, 'teacher': teachers[0] })
         form.fields['teacher']= forms.ModelChoiceField(queryset=
                                                        Persona.objects.
                                                        filter(performer_profile_id=owner.id))
-
         return render (request, 
                        'gbe/bid.tmpl',
                        {'forms':[form], 
-                        'page_title': page_title,                            
-                        'view_title': view_title, 
+                        'page_title': page_title,
+                        'view_title': view_title,
+                        'draft_fields': draft_fields
                         })
-                                
+
+    
 def edit_class(request, class_id):
     '''
     Edit an existing class.
@@ -618,30 +675,77 @@ def edit_class(request, class_id):
     except IndexError:
         return HttpResponseRedirect('/')   # no class for this id, fail out
     teachers = owner.personae.all()
+    draft_fields = Class().get_draft_fields
+
     if the_class.teacher not in teachers:
         return HttpResponseRedirect('/' )   # not a teacher for this class, fail out
 
     if request.method == 'POST':
-        form = ClassBidForm(request.POST)
+        if 'submit' in request.POST.keys():
+            form = ClassBidForm(request.POST, instance=the_class)
+        else:
+            form = ClassBidDraftForm(request.POST, instance=the_class)
+
         if form.is_valid():
-            new_class = form.save(commit=True)
+            the_class = form.save(commit=True)
+            if 'submit' in request.POST.keys():
+                if the_class.complete:
+                    the_class.submitted=True                    
+                    the_class.save()
+                    return HttpResponseRedirect("/")
+                else:
+                    return render (request, 
+                                   'gbe/bid.tmpl', 
+                                   {'forms':[form], 
+                                    'page_title': page_title,                            
+                                    'view_title': view_title,
+                                    'draft_fields': draft_fields,
+                                    'errors':['Cannot submit, class is not complete']})
+            the_class.save()
             return HttpResponseRedirect('/profile')
         else:
+            fields, requiredsub = Class().get_bid_fields
             return render (request, 
                            'gbe/bid.tmpl', 
                            {'forms':[form], 
-                        'page_title': page_title,                            
-                        'view_title': view_title, 
-                        })
+                            'page_title': page_title,                            
+                            'view_title': view_title, 
+                            'draft_fields': draft_fields,
+                            'submit_fields': requiredsub
+                       })
     else:
         form = ClassBidForm (instance=the_class)
+        draft_fields = Class().get_draft_fields
         return render (request, 
                        'gbe/bid.tmpl',
                        {'forms':[form], 
                         'page_title': page_title,                            
-                        'view_title': view_title, 
+                        'view_title': view_title,
+                        'draft_fields': draft_fields
                         })
 
+@login_required
+def view_class (request, class_id):
+    '''
+    Show a bid  which needs to be reviewed by the current user. 
+    To show: display all information about the bid, and a standard 
+    review form.
+    If user is not a reviewer, politely decline to show anything. 
+    '''
+    try:
+        classbid = Class.objects.filter(id=class_id)[0]
+        if classbid.teacher.contact != request.user.profile:
+          return HttpResponseRedirect('/')  # just fail for now    
+        classform = ClassBidForm(instance = classbid, prefix = 'The Class')
+        teacher = PersonaForm(instance = classbid.teacher, 
+                                prefix = 'The Teacher(s)')
+    except IndexError:
+        return HttpResponseRedirect('/')   # 404 please, thanks.
+    
+
+    return render (request, 'gbe/bid_view.tmpl',
+                   {'readonlyform': [classform, teacher]})
+    
 
 @login_required
 def review_class (request, class_id):
@@ -859,7 +963,7 @@ def create_vendor(request):
     except Profile.DoesNotExist:
         return HttpResponseRedirect("/accounts/profile/?next=vendor/create")
     if request.method == 'POST':
-        form = VendorBidForm(request.POST)
+        form = VendorBidForm(request.POST, request.FILES)
         if form.is_valid():
             vendor = form.save()
             return HttpResponseRedirect("/")
@@ -877,6 +981,107 @@ def create_vendor(request):
                        {'forms':[form], 
                         'page_title': title,
                         'view_title':title})
+
+@login_required
+def edit_vendor(request, vendor_id):
+
+    page_title = 'Edit Vendor Application'
+    view_title = 'Edit Your Vendor Application'
+    form = VendorBidForm(prefix='thebiz')
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        return HttpResponseRedirect('/accounts/profile/')   
+
+    try:
+        vendor = Vendor.objects.filter(id=vendor_id)[0]
+        if vendor.profile != profile:
+          return HttpResponseRedirect('/fail1')  # just fail for now 
+    except IndexError:
+        return HttpResponseRedirect('/fail2')  # just fail for now
+ 
+    if request.method == 'POST':
+        '''
+        If this is a formal submit request, then do all the checking.
+        If this is a draft, only a few fields are needed, use a form with fewer
+        required fields (same model)
+        '''
+        form = VendorBidForm(request.POST, request.FILES,
+                           instance=vendor, 
+                           prefix = 'thebiz')
+
+        if form.is_valid():
+            form.save()
+#            return HttpResponseRedirect('/wtf')
+        else:
+            return render (request,
+                           'gbe/bid.tmpl',
+                           {'forms':[form],
+                            'page_title': page_title,                            
+                            'view_title': view_title, 
+                       })
+
+        if 'submit' in request.POST.keys():
+            problems = vendor.validation_problems_for_submit()
+            if problems:
+                return render (request,
+                               'gbe/bid.tmpl',
+                               {'forms':[form], 
+                               'page_title': page_title,                            
+                               'view_title': view_title,
+                               'errors':problems})
+            else:
+                vendor.submitted = True
+
+                vendor.save()
+                details = {'user':request.user,
+                           'is_submission_fee':True,
+                           'bid':vendor}
+                return render(request, 
+                              'gbe/submission.tmpl',
+                              compute_submission(details)
+                              )
+
+        else:
+            return HttpResponseRedirect('/')
+    else:
+        if len(vendor.help_times.strip()) > 0:
+            help_times_initial = eval(vendor.help_times)
+        else:
+            help_times_initial = []
+        form = VendorBidForm(instance = vendor, 
+                           prefix='thebiz',
+                           initial = { 'help_times': help_times_initial })
+ 
+        return render (request, 
+                       'gbe/bid.tmpl',
+                       {'forms':[form],
+                        'page_title': page_title,                            
+                        'view_title': view_title,
+                        })
+                    
+@login_required
+def view_vendor (request, vendor_id):
+    '''
+    Show a bid  which needs to be reviewed by the current user. 
+    To show: display all information about the bid, and a standard 
+    review form.
+    If user is not a reviewer, politely decline to show anything. 
+    '''
+    try:
+        vendor = Vendor.objects.filter(id=vendor_id)[0]
+        if vendor.profile != request.user.profile:
+          return HttpResponseRedirect('/')  # just fail for now    
+        vendorform = VendorBidForm(instance = vendor, prefix = 'The Business')
+        profile = ParticipantForm(instance = vendor.profile, 
+                                prefix = 'The Contact Info')
+    except IndexError:
+        return HttpResponseRedirect('/')   # 404 please, thanks.
+    
+
+    return render (request, 'gbe/bid_view.tmpl',
+                   {'readonlyform': [vendorform, profile]})
+    
 
 @login_required
 def bid_response(request,type,response):
